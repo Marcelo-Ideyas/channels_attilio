ad261ce615a5a87a2f425b2504d2da1b7246dbe1
Marcelo Ortiz <marceloortiz@ideyas.it>
2019-12-18 16:11:11 -0300

    Done

--- src/cfehome/routing.py
+++ src/cfehome/routing.py
@@ -18,4 +18,13 @@ application = ProtocolTypeRouter({
     )
 })
 
+# application = ProtocolTypeRouter({
+#     # (http->django views is added by default)
+#     'websocket': AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
+
 #ws://ourdomain/<username>
--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -7,6 +7,11 @@ from channels.db import database_sync_to_async
 from .models import Thread, ChatMessage
 
 class ChatConsumer(AsyncConsumer):
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.keepalive = False
+
     async def websocket_connect(self, event):
         print("connected", event)
         # Após min 23
@@ -24,14 +29,13 @@ class ChatConsumer(AsyncConsumer):
         # é necessário utilizar await aqui, pois get_thread é uma função de sync que vira async
         thread_obj = await self.get_thread(me, other_user)
         print(me, thread_obj.id)
+        self.thread_obj = thread_obj ## Criação da thread_obj para ser utilizado no create
         chat_room = f"thread_{thread_obj.id}" # Chat room name
         self.chat_room = chat_room
-
         await self.channel_layer.group_add(
             chat_room,
             self.channel_name, # basic atribute for channel_layer
         )
-
         await self.send({
             "type":"websocket.accept"
         })
@@ -58,19 +62,30 @@ class ChatConsumer(AsyncConsumer):
             if user.is_authenticated:
                 username = user.username
             myResponse = {
+                # 'message': "This is a instance message",
                 'message': msg,
                 'username': username
             }
+            await self.create_chat_message(user, msg)
 
-            new_event = {
-                "type": "websocket.send",
-                "text": json.dumps(myResponse)
-            }
+            ### Comentado, pois a partir de 1:03 não é mais necessário um new_event
+            # new_event = {
+            #     "type": "websocket.send",
+            #     "text": json.dumps(myResponse)
+            # }
 
-            await self.channel_layer.group_send({
+            ### Por conta da mensagem nao ter chegado para o outro usuario, sera necessario utilizar um metodo diferente 1:00
+            
+            ## broadcasts the message event to be sent
+            await self.channel_layer.group_send(
                 self.chat_room,
-                new_event
-            })
+                # dict_key(dict_key[key])
+                {
+                    'type': 'chat_message',
+                    'message': json.dumps(myResponse)
+                }
+                # new_event
+            )
             # enviando a mensagem de volta pro front
             # await self.send({ # Comentado após inserir o Redis para o chat
             #     # "type":"websocket.close"
@@ -78,11 +93,35 @@ class ChatConsumer(AsyncConsumer):
             #     # "text":msg # Para enviar uma msg comum
             #     "text": json.dumps(myResponse) # Envia o dicionario myResponse
             # })
+
+    async def chat_message(self, event):
+        print('BEFORE')
+        print("message", event)
+        # send the actual message
+        await self.send({
+            "type": "websocket.send",
+            "text": event['message']
+        })
+
     async def websocket_disconnect(self, event):
         print("disconnect", event)
+        
 
     # Sync method
     # importante utilizar, verificar melhor o motivo
     @database_sync_to_async
     def get_thread(self, user, other_username):
         return Thread.objects.get_or_new(user, other_username)[0]
+
+    # me is the sender, msg is the msg
+    @database_sync_to_async
+    def create_chat_message(self, me, msg):
+        thread_obj = self.thread_obj
+        print('Salvando')
+        return ChatMessage.objects.create(
+                                    ### Para utilizar a thread_obj foi necessário acrescentar ao self thread.obj na linha 18
+                                    thread=thread_obj,
+                                    user=me,
+                                    message=msg
+                                )
+        print('Salvou')
